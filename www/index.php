607c15695534c40d8b0c96ff83eaab95acfbd3f6
Farrow Web Design <farrowwebdesign19@outlook.com>
2020-03-03 12:47:15 +0000

    Update index.php

--- www/index.php
+++ www/index.php
@@ -1,1 +1,27 @@
-<!--- Initial Commit --->
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <meta name="description" content="Description">
+    <meta name="keywords" content="KeyWords">
+    <meta name="author" content="Name">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+  </head>
+  <body>
+    <header>
+      <h1>Page Title</h1>
+      <nav>
+        <a href="#">Home</a>
+        <a href="#">About Us</a>
+        <a href="#">Page</a>
+        <a href="#">Contact Us</a>
+      </nav>
+    </header>
+    <main>
+    </main>
+    <footer>
+    </footer>
+  </body>
+</html>
